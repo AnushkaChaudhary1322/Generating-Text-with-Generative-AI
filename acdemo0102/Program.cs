7080f4abd177912fc19207ac75b461050e55edd3
Anushka1322 <122356104+AnushkaChaudhary1322@users.noreply.github.com>
2026-02-01 09:39:50 +0000

    generate text

---
+++ acdemo0102/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenAI_API;
+using OpenAI_API.Completions;
+using OpenAI_API.Models;
+
+Console.WriteLine("Hello, World!");
+
+var api = new OpenAIAPI("YOUR_API_KEY_HERE");
+
+var prompt = "A long time ago, in a galaxy far, far away..";
+
+var request = new CompletionRequest
+{
+    Prompt = prompt,
+    Model = Model.DavinciText,
+    MaxTokens = 100,
+    Temperature = 0.7
+};
+
+var response = await api.Completions.CreateCompletionAsync(request);
+
+Console.WriteLine("Generated Text:");
+Console.WriteLine(response.Completions[0].Text);
+
+
+
